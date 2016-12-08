8edadd0bdcfc817c201fd2d1a4c1df890f55e79b
axflores <aflores67@mail.csuchico.edu>
2016-12-08 00:32:44 -0800

    Add files via upload

---
+++ songlist.js
@@ -0,0 +1,51 @@
+window.onload = loadList;
+var current = 0;
+var allSongs = [];
+
+//Prototype for Song objects
+function Song(title,artist,album,year)
+{
+  this.song = title;
+  this.artist = artist;
+  this.album = album;
+  this.year = year;
+  this.toString = function(){
+    return this.song + " by " + this.artist;
+  }
+  this.releaseInfo = function(){
+    return "Released on " + this.album + " in " + this.year;
+  };
+  this.listened = 0;
+  this.play = function(){
+    this.listened++;
+  };
+  this.count = function(){
+    return this.listened;
+  };
+}
+
+//Adds new instances of songs to the list
+function loadList()
+{
+  allSongs.push(new Song("Hey Jude","The Beatles","Let It Be",1970));
+  allSongs.push(new Song("Lodi Dodi","Snoop Dogg","Doggystyle",1993));
+  allSongs.push(new Song("Get Lucky","Daft Punk","Random Access Memories",2013));
+  allSongs.push(new Song("Inevitable","Shakira","Dónde Están los Ladrones?",1998));
+  allSongs.push(new Song("Wagon Wheel","Old Crow Medicine Show","Old Crow Medicine Show",2004));
+  displaySong();
+}
+
+//Progresses to the next song to the list, or if the end of the list has been
+//reached, then loop back to the beginning
+function nextSong()
+{
+  current = (current+1)%allSongs.length;
+  displaySong();
+}
+
+//Show song's information in the "nowPlaying" span
+function displaySong()
+{
+  document.getElementById("nowPlaying").innerHTML = allSongs[current].toString();
+  document.getElementById("showinfo").innerHTML = allSongs[current].releaseInfo();
+}
